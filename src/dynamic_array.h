bfc6f02e531f22fb17f3b3070cab57107b58527a
edd255 <abaddon@disroot.org>
2023-01-19 09:05:16 +0100

    refactor(dynamic_array): moved to header file

--- src/dynamic_array.h
+++ src/dynamic_array.h
@@ -1,16 +1,59 @@
+#include <iostream>
+
 template <typename T>
 class DynamicArray
 {
-        public:
-                void reallocate();
-                void push_back(T element);
-                void pop_back();
-                T get(int index);
-                DynamicArray(int max_size);
-                ~DynamicArray();
+    private:
+        int current_size;
+        int max_size;
+        T* array;
 
-        private:
-                int current_size;
-                int max_size;
-                T* array;
+    public:
+        void reallocate()
+        {
+            max_size = 2 * current_size;
+            T new_array = new T[max_size];
+            for (int i = 0; i < array; i++) {
+                new_array[i] = array[i];
+            }
+            free(array);
+            array = new_array;
+        }
+
+        void push_back(T element)
+        {
+            if (current_size == max_size) {
+                reallocate();
+            }
+            array[current_size] = element;
+            current_size++;
+        }
+
+        void pop_back()
+        {
+            current_size--;
+            if (current_size == max_size / 4 && current_size > 0) {
+                reallocate();
+            }
+        }
+
+        T get(int index)
+        {
+            if (index < 0 || index >= max_size) {
+                return NULL;
+            }
+            return array[index];
+        }
+
+        DynamicArray(int max_size)
+        {
+            this -> array        = new T[max_size];
+            this -> max_size     = max_size;
+            this -> current_size = 0;
+        }
+
+        ~DynamicArray()
+        {
+            delete[] array;
+        }
 };
